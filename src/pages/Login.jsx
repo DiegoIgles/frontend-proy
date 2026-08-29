7a5300b69916da61d1bbc39f4a9b432b7ae34b8f
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2026-08-29 12:51:07 -0400

    logov1 oficial added in all system

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ import FloatingChatbot from "../components/FloatingChatbot";
 /* Dos versiones del mismo lockup: la de wordmark blanco solo lee sobre el navy
    del panel izquierdo; la de color, solo sobre el fondo claro del formulario. */
 import logoBlanco from "../assets/brand/enerlogic_v2_transparent.png";
-import logoColor from "../assets/brand/enerlogic-color.png";
+import logoColor from "../assets/brand/enerlogic_v1_oficial_transparent.png";
 
 function Login() {
   const { user, loading, login } = useAuth();
